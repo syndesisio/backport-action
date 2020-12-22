88763eb2e1879084dfa5148404f1736c596603c9
Zoran Regvart <zregvart@apache.org>
2020-12-22 21:30:12 +0100

    fix: working fail comments

--- spec/backport_spec.sh
+++ spec/backport_spec.sh
@@ -123,13 +123,7 @@ error: could not apply *... Merge commit '*'
 hint: after resolving the conflicts, mark the corrected paths
 hint: with 'git add <paths>' or 'git rm <paths>'
 hint: and commit the result with 'git commit'\n\`\`\`
-CURL invoked with:-XPOST -fsSL --output /dev/null -H Accept: application/vnd.github.v3+json -H Authorization: Bearer github-token -H Content-Type: application/json -d {    \"body\": \"Unable to cherry-pick commit * on top of branch 'branch'.\n\nThis pull request needs to be backported manually.\nError:\n\`\`\`\nAuto-merging file
-CONFLICT (content): Merge conflict in file
-error: could not apply *... Merge commit '*'
-hint: after resolving the conflicts, mark the corrected paths
-hint: with 'git add <paths>' or 'git rm <paths>'
-hint: and commit the result with 'git commit'\n\`\`\`\"
-  } comments-url"
+CURL invoked with:-XPOST -fsSL --output /dev/null -H Accept: application/vnd.github.v3+json -H Authorization: Bearer github-token -H Content-Type: application/json -d {\"body\":\"Unable to cherry-pick commit * on top of branch 'branch'.\\n\\nThis pull request needs to be backported manually.\\nError:\\n\`\`\`\\nAuto-merging file\\nCONFLICT (content): Merge conflict in file\\nerror: could not apply *... Merge commit '*'\\nhint: after resolving the conflicts, mark the corrected paths\\nhint: with 'git add <paths>' or 'git rm <paths>'\\nhint: and commit the result with 'git commit'\\n\`\`\`\"} comments-url"
         The status should equal 1
       End
     End
