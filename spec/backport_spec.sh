a176d4fc6ee40a46c5950ab58e73e8139048060b
Zoran Regvart <zregvart@apache.org>
2021-01-05 17:42:29 +0100

    chore: remove quotes, use user API instead of rate

--- spec/backport_spec.sh
+++ spec/backport_spec.sh
@@ -267,7 +267,7 @@ EOF
 
       It 'Deletes branches'
         When call delete_branch backport/123-to-branch
-        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H 'Authorization: Bearer ${INPUT_TOKEN}' git-refs-url/heads/backport/123-to-branch"
+        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H Authorization: Bearer ${INPUT_TOKEN} git-refs-url/heads/backport/123-to-branch"
       The output should equal '::group::Deleting closed pull request branch
 ::debug::status=204
 Deleted
@@ -287,7 +287,7 @@ Deleted
 
       It 'Doesn''t fail on deleted branches'
         When call delete_branch backport/123-to-branch
-        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H 'Authorization: Bearer ${INPUT_TOKEN}' git-refs-url/heads/backport/123-to-branch"
+        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H Authorization: Bearer ${INPUT_TOKEN} git-refs-url/heads/backport/123-to-branch"
         The output should equal '::group::Deleting closed pull request branch
 ::debug::status=422
 Deleted
@@ -312,7 +312,7 @@ Deleted
 
       It 'Fails to delete branch'
         When run delete_branch backport/123-to-branch
-        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H 'Authorization: Bearer ${INPUT_TOKEN}' git-refs-url/heads/backport/123-to-branch"
+        The value "$(cat "${curl_args}")" should equal "-XDELETE -v -fsL --fail --output /dev/stderr -w %{http_code} -H Accept: application/vnd.github.v3+json -H Authorization: Bearer ${INPUT_TOKEN} git-refs-url/heads/backport/123-to-branch"
         The output should equal '::group::Deleting closed pull request branch
 ::debug::status=401
 Failed to delete branch
@@ -507,15 +507,15 @@ EOF
 
         After "rm \"${curl_args}\""
 
-        It 'Fails when rate API returns status 4xx'
+        It 'Fails when user API returns status 4xx'
           When run check_token
           The output should equal '::group::Checking token
 ::debug::curl verbose output
 ::debug::curl verbose output (second line)
 ::debug::status=401
-::error::Provided INPUT_TOKEN is not valid according to the rate API
+::error::Provided INPUT_TOKEN is not valid according to the user API
 ::endgroup::'
-          The value "$(cat "${curl_args}")" should equal '-v -fsL --fail --output /dev/stderr -w %{http_code} -H '"'"'Authorization: Bearer github-token'"'"' https://api.github.com/rate_limit'
+          The value "$(cat "${curl_args}")" should equal '-v -fsL --fail --output /dev/stderr -w %{http_code} -H Authorization: Bearer github-token https://api.github.com/user'
           The status should equal 1
         End
 
@@ -528,7 +528,7 @@ EOF
           exit 0
         }
 
-        It 'Succeeds when rate API returns status 2xx'
+        It 'Succeeds when user API returns status 2xx'
           When run check_token
           The output should equal '::group::Checking token
 ::debug::curl verbose output
@@ -536,7 +536,7 @@ EOF
 ::debug::status=200
 Token seems valid
 ::endgroup::'
-          The value "$(cat "${curl_args}")" should equal '-v -fsL --fail --output /dev/stderr -w %{http_code} -H '"'"'Authorization: Bearer github-token'"'"' https://api.github.com/rate_limit'
+          The value "$(cat "${curl_args}")" should equal '-v -fsL --fail --output /dev/stderr -w %{http_code} -H Authorization: Bearer github-token https://api.github.com/user'
           The status should equal 0
         End
       End
